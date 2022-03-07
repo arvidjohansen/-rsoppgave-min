caa4f5b23909aae85642a97877479f9bf2a2a495
Arvid <biskus@gmail.com>
2022-03-07 11:15:41 +0100

    Added black and white background onmousedown

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 
-
-console.log("hei");
-
-
 var cookieEl = document.getElementById("cookie");
 
 var clickCounter = 0;
+var clickCounterEl = document.getElementById("clickCounter");
 
 function click(e){
     clickCounter += 1;
-    document.getElementById("clickCounter").innerHTML = clickCounter;
+    clickCounterEl.innerHTML = clickCounter;
 }
 
+function mousedown(e){
+    cookieEl.style.backgroundColor = "black";
+}
+
+function mouseup(e){
+    cookieEl.style.backgroundColor = "white";
+}
+
+cookieEl.addEventListener("mousedown",mousedown);
+cookieEl.addEventListener("mouseup",mouseup);
 cookieEl.addEventListener("click",click);
+
+//cookieEl.addEventListener
