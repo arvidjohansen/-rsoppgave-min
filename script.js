e5c35581cf20c319baea459ab0048430a76f7fa0
Arvid <biskus@gmail.com>
2022-03-10 10:29:31 +0100

    Added new buttons

--- script.js
+++ script.js
@@ -3,22 +3,43 @@ var cookieEl = document.getElementById("cookie");
 
 var clickCounter = 0;
 var clickCounterEl = document.getElementById("clickCounter");
+var pointPerClick = 1;
+var pointPerClickEl = document.getElementById("pointPerClick");
+var powerup1El = document.getElementById("cookiePowerup1");
+var powerup2El = document.getElementById("cookiePowerup2");
 
 function click(e){
-    clickCounter += 1;
+    clickCounter += pointPerClick;
     clickCounterEl.innerHTML = clickCounter;
 }
+function powerup1(e){
+    pointPerClick += 2;
+    pointPerClickEl.innerHTML = pointPerClick;
+}
+function powerup2(e){
+    pointPerClick += 10;
+    pointPerClickEl.innerHTML = pointPerClick;
+}
 
+cookieEl.addEventListener("click",click);
+powerup1El.addEventListener("click",powerup1);
+powerup2El.addEventListener("click",powerup2);
+
+
+//Make background flash when clicked
 function mousedown(e){
     cookieEl.style.backgroundColor = "black";
 }
-
 function mouseup(e){
     cookieEl.style.backgroundColor = "white";
 }
-
 cookieEl.addEventListener("mousedown",mousedown);
 cookieEl.addEventListener("mouseup",mouseup);
-cookieEl.addEventListener("click",click);
+powerup1El.addEventListener("mousedown", function(e){
+    powerup1El.style.backgroundColor = "black";
+})
+powerup1El.addEventListener("mouseup", function(e){
+    powerup1El.style.backgroundColor = "white";
+})
 
-//cookieEl.addEventListener
+
